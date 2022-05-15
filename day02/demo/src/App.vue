71404a139324edcd61a7df4b9f7d9f7bde9f0028
大聪明 <2119299531@qq.com>
2022-05-15 17:47:31 +0800

    添加一些注释

--- day02/demo/src/App.vue
+++ day02/demo/src/App.vue
@@ -1,172 +1,39 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-12 pt-3">
-        <table class="table table-bordered">
-          <thead>
-            <tr>
-              <th scope="col">编号</th>
-              <th scope="col">资产名称</th>
-              <th scope="col">价格</th>
-              <th scope="col">创建时间</th>
-              <th scope="col">操作</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="item in list" :key="item.id">
-              <th scope="row">{{ item.id }}</th>
-              <td>{{ item.name }}</td>
-              <td :class="{ expensive: item.price > 100 }">{{ item.price }}</td>
-              <td>{{ item.time | dateFilter }}</td>
-              <td>
-                <!-- 1.给删除按钮绑定事件，获取要删除的元素索引 -->
-                <button
-                  type="button"
-                  class="btn btn-link"
-                  @click="deleteProduct(index)"
-                >
-                  删除
-                </button>
-              </td>
-            </tr>
-            <tr class="bg-light">
-              <th scope="row">统计</th>
-              <td colspan="2">总价: {{ totalPrice }}</td>
-              <td colspan="2">均价：{{ random }}</td>
-            </tr>
-          </tbody>
-          <!-- 第四步，控制空状态的显示 -->
-          <!-- <tfoot style="display: none"> -->
-          <tfoot v-show="list.length === 0">
-            <tr>
-              <td class="text-center" colspan="5">暂无数据</td>
-            </tr>
-          </tfoot>
-        </table>
-      </div>
-    </div>
-
-    <form class="row align-items-center">
-      <div class="col-3">
-        <!-- 添加功能：给表单v-model绑定vue变量收集用户输入内容 -->
-        <input
-          type="text"
-          class="form-control"
-          placeholder="资产名称"
-          v-model="productName"
-        />
-      </div>
-
-      <div class="col-3">
-        <input
-          type="text"
-          class="form-control"
-          placeholder="价格"
-          v-model.number="productPrice"
-        />
-      </div>
-      <!-- 添加功能：添加资产按钮 – 绑定点击事件 -->
-      <!-- .prevent阻止按钮提交表单刷新页 -->
-      <div class="col-3">
-        <button
-          type="submit"
-          class="btn btn-primary"
-          @click.prevent="addProperty"
-        >
-          添加资产
-        </button>
-      </div>
-    </form>
+  <div>
+    <!-- 过滤器:转换数据格式 -->
+    商品价格: {{ price | priceFilter }}
+    <h2>{{ "hello" | toUpperCase }}</h2>
+    <!-- 过滤器能使用在插值表达式和v-bind属性里 -->
+    <p :title="'hello vue' | toUpperCase">hello vue</p>
+    <!-- 第2种写法 -->
+    <p>{{ hi | reserseFn }}</p>
+    <p>{{ 1 | moneyFilter("￥") }}</p>
+    <p>{{ 1 | moneyFilter("元") }}</p>
+    <p>{{ 1 | moneyFilter("$", "#") }}</p>
   </div>
 </template>
 
 <script>
-import moment from "moment";
 export default {
-  name: "App",
   data() {
     return {
-      list: [
-        {
-          id: 100,
-          name: "外套",
-          price: 199,
-          time: new Date("2010-08-12"),
-        },
-        {
-          id: 101,
-          name: "裤子",
-          price: 34,
-          time: new Date("2013-09-01"),
-        },
-        {
-          id: 102,
-          name: "鞋",
-          price: 25.4,
-          time: new Date("2018-11-22"),
-        },
-        {
-          id: 103,
-          name: "头发",
-          price: 19900,
-          time: new Date("2020-12-12"),
-        },
-      ],
-      productName: "",
-      productPrice: 0,
+      price: 0,
+      hi: "hellow",
     };
   },
-  computed: {
-    totalPrice() {
-      let total = 0;
-      this.list.forEach((item) => {
-        total += item.price;
-      });
-      return total;
-    },
-    random() {
-      return this.totalPrice / this.list.length;
-    },
-  },
+  // 声明在data同级下的filters对象里
   filters: {
-    dateFilter(time) {
-      return moment(time).format("YYYY-MM-DD");
+    // 过滤器要声明为方法
+    // 表达式的值是过滤器的第一个参数
+    priceFilter(num) {
+      return `${num < 10 ? "0" + num : num}￥`;
     },
-  },
-  methods: {
-    addProperty() {
-      // 判断用户内容是否符合规定
-      if (!this.productName || !this.productPrice) {
-        alert("资产名称和价格不能为空");
-        return;
-      }
-      // 5.处理空数据情况下的新增逻辑异常
-      let id;
-      if (this.list.length > 0) {
-        id = this.list[this.list.length - 1].id + 1;
-      } else {
-        id = 100;
-      }
-      // 添加数组到数组中
-      this.list.push({
-        // 数组取最后一个元素
-        id,
-        name: this.productName,
-        price: this.productPrice,
-        time: new Date(),
-      });
-    },
-    deleteProduct(index) {
-      // 3.删除元素
-      this.list.splice(index, 1);
+    toUpperCase(value) {
+      return value.toUpperCase();
     },
   },
 };
 </script>
 
-<style scoped>
-  /* 大于100价格, 动态设置red类 */
-.expensive {
-  color: red;
-}
+<style>
 </style>
--- day02/demo/src/main.js
+++ day02/demo/src/main.js
@@ -4,6 +4,13 @@ import "../node_modules/bootstrap/dist/css/bootstrap.css"
 
 Vue.config.productionTip = false
 
+Vue.filter("reserseFn", function(value) {
+  return value.split("").reverse().join("");
+});
+Vue.filter("moneyFilter", function(value, unit, unit1) {
+  return `${value} ${unit} ${unit1 || ''}`
+});
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
