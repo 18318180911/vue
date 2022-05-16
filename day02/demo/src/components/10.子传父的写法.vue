fe50aa49cdc23379a57ccef305614ca02a043b88
大聪明 <2119299531@qq.com>
2022-05-16 16:43:45 +0800

    子传父写法

---
+++ day02/demo/src/components/10.子传父的写法.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <div class="item">
+          {{title}}
+          <div>只收{{price}}</div>
+          <div>{{info}}</div>
+          <button @click="handleClick">砍价</button>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['title', 'price', 'info', 'index'],
+    methods: {
+        handleClick() {
+            console.log('降价');
+            console.log(this.price);
+            this.$emit('kanjia', this.index, 3)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
