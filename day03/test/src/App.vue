12ffb5a496f9e6032c2a46d42d283384c767923e
大聪明 <2119299531@qq.com>
2022-05-18 20:25:12 +0800

    日期格式化年月日形式

--- day03/test/src/App.vue
+++ day03/test/src/App.vue
@@ -1,47 +1,105 @@
 <template>
-  <div>
-    <p>
-      来自于：<select v-model="ctiy">
-        <option value="湛江">湛江</option>
-        <option value="广州">广州</option>
-        <option value="深圳">深圳</option>
-      </select>
-    </p>
-    <!-- checkbox多选的时候，要把v-model绑定的变量声明为数组 -->
-    <!-- 单选的时候v-model变量为非数组，绑定的是checked属性 -->
-    <p>
-      爱好：<input type="checkbox" value="打王者" v-model="hobby">打王者
-      <input type="checkbox" value="睡觉" v-model="hobby">睡觉
-      <input type="checkbox" value="辣条" v-model="hobby">辣条
-    </p>
-    <p>
-      <!-- 单选的时候v-model变量为非数组，绑定的是checked属性 -->
-      <input type="checkbox" v-model="agree"> 用户是否同意
-    </p>
-    <p>
-      性别：<input type="radio" value="男" v-model="gender">男
-      <input type="radio" value="女" v-model="gender">女
-    </p>
-    <p>
-      自我介绍：<textarea v-model="info"></textarea>
-    </p>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 pt-3">
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+              <th scope="col">编号</th>
+              <th scope="col">资产名称</th>
+              <th scope="col">价格</th>
+              <th scope="col">创建时间</th>
+              <th scope="col">操作</th>
+            </tr>
+          </thead>
+          <tbody>
+            <!-- 1.使用v-for遍历数组，渲染数据 -->
+            <tr v-for="item in list" :key="item.id">
+              <th scope="row">{{item.id}}</th>
+              <td>{{item.name}}</td>
+              <!-- 2.价格大于100显示红色 -->
+              <td :class="{color: item.price > 100}">{{item.price}}</td>
+              <td>{{item.time  | dateFilter}}</td>
+              <td>
+                <button type="button" class="btn btn-link">删除</button>
+              </td>
+            </tr>
+            <tr class="bg-light">
+              <th scope="row">统计</th>
+              <td colspan="2">总价：0</td>
+              <td colspan="2">均价：0</td>
+            </tr>
+          </tbody>
+          <tfoot style="display: none">
+            <tr>
+              <td class="text-center" colspan="5">暂无数据</td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+    </div>
+
+    <form class="row align-items-center">
+      <div class="col-3">
+        <input type="text" class="form-control" placeholder="资产名称" />
+      </div>
+
+      <div class="col-3">
+        <input type="text" class="form-control" placeholder="价格" />
+      </div>
+
+      <div class="col-3">
+        <button type="submit" class="btn btn-primary">添加资产</button>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
+import moment from 'moment'
 export default {
+  name: "App",
   data() {
     return {
-      ctiy: '',
-      hobby: [],
-      agree: false,
-      gender: '',
-      info: '',
+      list: [
+        {
+          id: 100,
+          name: "外套",
+          price: 199,
+          time: new Date("2010-08-12"),
+        },
+        {
+          id: 101,
+          name: "裤子",
+          price: 34,
+          time: new Date("2013-09-01"),
+        },
+        {
+          id: 102,
+          name: "鞋",
+          price: 25.4,
+          time: new Date("2018-11-22"),
+        },
+        {
+          id: 103,
+          name: "头发",
+          price: 19900,
+          time: new Date("2020-12-12"),
+        },
+      ],
+    };
+  },
+  // filters过滤器
+  filters: {
+    dateFilter(time) {
+      return moment(time).format('YYYY-MM-DD')
     }
   }
-}
+};
 </script>
 
-<style>
-
+<style scoped>
+.color{
+  color: red;
+}
 </style>
