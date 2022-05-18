5a049c162d16516c8f647b42780f3df073d7a69b
大聪明 <2119299531@qq.com>
2022-05-18 11:55:56 +0800

    品牌管理模板

---
+++ day03/test/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>{{content}}</h1>
+    <button @click="prot">逆转世界</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      content: 'Hello, World'
+    }
+  },
+  methods: {
+    prot() {
+      const arr = this.content.split('');
+      arr.reverse();
+      this.content = arr.join('');
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ day03/test/src/components/demo1.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>{{mgs}}</h1>
+    <button @click="online">逆转世界</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      mgs:'Hello, World'
+    }
+  },
+  methods: {
+    online() {
+      const array = this.mgs.split("");
+      array.reverse();
+      this.mgs = array.join("");;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
