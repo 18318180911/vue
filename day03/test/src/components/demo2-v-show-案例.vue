55dc4ec861016bd9b1301a63df1e4da358d78085
大聪明 <2119299531@qq.com>
2022-05-18 16:45:22 +0800

    v-show和v-model

---
+++ day03/test/src/components/demo2-v-show-案例.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>案例：折叠面板</h1>
+    <h2>
+      芙蓉楼送辛渐
+      <button @click="put">{{ show ? "收起" : "展开" }}</button>
+    </h2>
+    <div v-show="show">
+      <p>寒雨连江夜入吴</p>
+      <p>平明送客楚山孤</p>
+      <p>洛阳亲友如相问</p>
+      <p>一片冰心在玉壶</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: true,
+    };
+  },
+  methods: {
+    put() {
+      // 取反
+      this.show = !this.show;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
