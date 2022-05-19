06711d063942163426561cdf98c0f8062cf8dce9
大聪明 <2119299531@qq.com>
2022-05-19 16:45:37 +0800

    $refs的实际运用

---
+++ day03/test/src/components/test2/MyBox.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      我的盒子{{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            msg: '666'
+        }
+    },
+    methods: {
+        dajiao() {
+            alert('是组件')
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
