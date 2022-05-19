01e7161e04e05c905cec70f86c53fbc92a0726ce
大聪明 <2119299531@qq.com>
2022-05-19 17:16:15 +0800

    输入框显示后自动聚焦的例子

--- day03/test/src/components/test2/demo8-refs的实际运用.vue
+++ day03/test/src/components/test2/demo8-refs的实际运用.vue
@@ -9,7 +9,9 @@ import MyBox from '@/components/test2/MyBox.vue'
 export default {
   components: {MyBox},
   mounted() {
+    // 用id获取组件
     console.log(document.getElementById('zjBox'));
+    // 用ref获取组件
     console.log(this.$refs.zjBoxRef);
     setTimeout(() => {
       this.$refs.zjBoxRef.msg = '888'
