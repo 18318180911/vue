ea0d9b722da10d2de0163f7ed20acba01ea33440
大聪明 <2119299531@qq.com>
2022-05-19 12:18:20 +0800

    get请求带参数

--- day03/test/src/components/test2/demo2-axios的使用.vue
+++ day03/test/src/components/test2/demo2-axios的使用.vue
@@ -9,10 +9,17 @@ import axios from 'axios'
 export default {
   methods: {
     addBooks() {
+    // axios 发请求只需要带上一个配置对象
       axios({
+          // url 地址
         url: 'http://123.57.109.30:3006/api/getbooks',
-        method: 'get'
+        // method 请求方法
+        method: 'get',
+        params: {
+            bookname: this.bookName
+        }
       }).then(res => {
+        // 这个库有个特点, 会将你发请求的配置放在返回中一起交给你, 最终的数据, 其实是放在 res.data
         console.log(res.data);
       })
     }
