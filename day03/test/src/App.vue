2f78ce4440b56efd0be1428d87752d1db4d0b4ad
大聪明 <2119299531@qq.com>
2022-05-19 10:42:16 +0800

    水果案例-父传子，子传父

--- day03/test/src/App.vue
+++ day03/test/src/App.vue
@@ -2,26 +2,34 @@
   <div>
     <h4>水果案例</h4>
     <input type="text" @keyup.enter="login" v-model="fruits">
-    <p>当前被点击是：{{fruit}}</p>
+    <p>当前被点击是：{{currentFruit}}</p>
     <ul>
-      <li v-for="(item, index) in list" :key="index" @click="fruit = item">{{item}}</li>
+      <MyFruit v-for="(item, index) in list" :key="index" :data="item" @clickFruit="changCurrent"/>
     </ul>
   </div>
 </template>
 
 <script>
+import MyFruit from '@/components/MyFruit.vue'
 export default {
+  components: {
+    MyFruit
+  },
   data() {
     return {
       fruits: '',
       list: [],
       fruit: '',
+      currentFruit:'',
     }
   },
   methods: {
     login() {
       this.list.push(this.fruits);
       this.fruits = '';
+    },
+    changCurrent(data) {
+      this.currentFruit = data
     }
   }
 }
