3721dd2b1ee1e8b1a11c7ba29ee3e8d7cb487648
大聪明 <2119299531@qq.com>
2022-05-20 19:46:52 +0800

    扩展生命周期

--- day03/test/src/components/test4/UserInfo.vue
+++ day03/test/src/components/test4/UserInfo.vue
@@ -7,7 +7,21 @@
 
 <script>
 export default {
-
+  created() {
+    console.log('我被创建了');
+  },
+  mounted() {
+    console.log('被挂载了');
+  },
+  destroyed() {
+    console.log('被销毁了');
+  },
+  activated() {
+    console.log("激活时触发");
+  },
+  deactivated() {
+    console.log("失去激活状态触发")
+  }
 }
 </script>
 
