904400b72565263b3623535d77f8cba26ac739bf
大聪明 <2119299531@qq.com>
2022-05-20 16:01:51 +0800

    动态组件

---
+++ day03/test/src/components/test4/UserInfo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <input type="text" name="" id="" placeholder="姓名">
+      <input type="text" placeholder="年龄">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
