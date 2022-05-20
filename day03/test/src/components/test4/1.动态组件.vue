99ae6e164101edd366a217ffec12f6000a44065c
大聪明 <2119299531@qq.com>
2022-05-20 16:05:36 +0800

    提交动态组件

---
+++ day03/test/src/components/test4/1.动态组件.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+      <!-- 动态组件 -->
+    <button @click="componentsName = 'UserInfo'">个人信息</button>
+    <button @click="componentsName = 'UserName'">账号登陆</button>
+    <!-- 组件缓存 -->
+    <keep-alive>
+        <component :is="componentsName"></component>
+    </keep-alive>
+    
+  </div>
+</template>
+
+<script>
+import UserInfo from "@/components/test4/UserInfo.vue";
+import UserName from "@/components/test4/UserName.vue";
+export default {
+  data() {
+    return {
+      componentsName: "UserInfo",
+    };
+  },
+  components: {
+    UserInfo,
+    UserName,
+  },
+};
+</script>
+
+<style>
+</style>
