f332002cf1c209909738911620416bc76e46781f
大聪明 <2119299531@qq.com>
2022-05-20 22:38:00 +0800

    插槽作用域使用场景

---
+++ day03/test/src/components/test4/MyTable.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <table>
+      <tbody>
+        <tr v-for="(item, index) in data" :key="index">
+          <!-- <td>{{ item.name }}</td>
+          <td>{{ item.age }}</td>
+          <td><img :src="item.headImgUrl" alt="" /></td> -->
+          <slot :row="item" :index="index"/>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['data']
+}
+</script>
+
+<style>
+
+</style>
