ae519810b97ab31b915b06df9b6085a74cd2f8c6
大聪明 <2119299531@qq.com>
2022-05-23 15:45:03 +0800

    路由基本配置使用

--- day03/test/src/main.js
+++ day03/test/src/main.js
@@ -5,9 +5,31 @@ import './assets/fonts/iconfont.css'
 // 引入库
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
-const router = new VueRouter({})
+// 3. 创建一个实例
+// 3.1 创建路由配置表是个数组, 里面的每个元素都是对象
+// 配置路由方式
+// 1 引入组件对象 2 指定对应路径
+import Find from './components/test7/Find.vue'
+import My from './components/test7/My.vue'
 Vue.config.productionTip = false
-
+const routes = [
+  {
+    // 1. path 路径 / 开头
+    path: '/find',
+    // 2. component 组件
+    component: Find
+  },
+  {
+    path: '/my',
+    component: My
+  }
+]
+// 3.2 创建路由实例, 放入配置表
+const router = new VueRouter({
+  // 这里可以有配置对象
+  // 有个属性叫routes是路由配置表,指定 url=>组件的配对
+  routes
+})
 new Vue({
   router,
   render: h => h(App),
