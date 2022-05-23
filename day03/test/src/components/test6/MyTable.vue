8f2c87fffee68c6e723ce9f796655964bd60153e
大聪明 <2119299531@qq.com>
2022-05-23 11:08:44 +0800

    获取数据

--- day03/test/src/components/test6/MyTable.vue
+++ day03/test/src/components/test6/MyTable.vue
@@ -1,5 +1,28 @@
 <template>
-  
+  <div>
+      <table class="table table-bordered table-strpped">
+          <!-- 表格标题区域 -->
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>商品名称</th>
+        <th>价格</th>
+        <th>标签</th>
+        <th>操作</th>
+      </tr>
+    </thead>
+    <!-- 表格主体区域 -->
+    <tbody>
+      <tr >
+        <td>1</td>
+        <td>商品</td>
+        <td>998</td>
+        <td>xxx</td>
+        <td>xxx</td>
+      </tr>
+    </tbody> 
+      </table>
+  </div>
 </template>
 
 <script>
@@ -8,6 +31,10 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped lang="less">
+.my-goods-list {
+  .badge {
+    margin-right: 5px;
+  }
+}
 </style>
--- day03/test/src/components/views/MyGoodsList.vue
+++ day03/test/src/components/views/MyGoodsList.vue
@@ -1,10 +1,31 @@
 <template>
-  <div>列表</div>
+  <!-- <div>列表</div> -->
+  <div>
+    <MyTable />
+  </div>
 </template>
 
 <script>
+import MyTable from '@/components/test6/MyTable.vue'
+import axios from 'axios'
+axios.defaults.baseURL = 'https://applet-base-api-t.itheima.net/'
 export default {
-
+  components: {
+    MyTable
+  },
+  data() {
+    return {
+      list: []
+    }
+  },
+  created() {
+    axios({
+      url: '/api/goods'
+    }).then(res => {
+      console.log(res.data.data);
+      this.list = res.data.data
+    })
+  }
 }
 </script>
 
