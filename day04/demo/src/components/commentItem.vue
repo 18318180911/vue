ab02ea9f9880ffbc19b5d6d40b0787a1bb789073
大聪明 <2119299531@qq.com>
2022-05-30 11:42:40 +0800

    封装嵌套内容

---
+++ day04/demo/src/components/commentItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- 嵌套内容 -->
+  <div class="commentItem">
+    <div class="top">
+      <div class="left">
+        <span>{{ item.user.nickname }}</span> &nbsp;&nbsp;&nbsp;
+        <span>2分钟前</span>
+      </div>
+      <span>回复</span>
+    </div>
+    <div class="bottom">{{ item.content }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        item: {
+            type: Object,
+            require: true
+        }
+    }
+};
+</script>
+
+<style>
+</style>
--- day04/demo/src/views/commentList.vue
+++ day04/demo/src/views/commentList.vue
@@ -4,15 +4,16 @@
     <van-nav-bar title="精彩评论" left-arrow @click-left="$router.back()" />
     <!-- 评论列表 -->
     <div class="item" v-for="item in comment" :key="item.id">
-        <div class="head">
-            <img :src="item.user.head_img | joinPath" alt />
-            <div>
-                <p>{{item.user.nickname}}</p>
-                <span v-formatDate="item.create_date"></span>
-            </div>
-            <span>回复</span>
+      <div class="head">
+        <img :src="item.user.head_img | joinPath" alt />
+        <div>
+          <p>{{ item.user.nickname }}</p>
+          <span v-formatDate="item.create_date"></span>
         </div>
-        <div class="text">{{item.content}}</div>
+        <span>回复</span>
+      </div>
+      <commentItem  v-if="item.parent" :item="item.parent"></commentItem>
+      <div class="text">{{ item.content }}</div>
     </div>
     <!-- 底部评论块 -->
     <commentFooter></commentFooter>
@@ -21,62 +22,79 @@
 
 
 <script>
-import {post_comment} from '@/api/news'
+import { post_comment } from "@/api/news";
+import commentItem from '@/components/commentItem.vue'
 export default {
-    data() {
-        return {
-            comment: []
-        }
-    },
-    created() {
-        post_comment(this.$route.query.id).then(res => {
-            this.comment = res.data.data
-        })
-    }
+  data() {
+    return {
+      comment: [],
+      components: { commentItem },
+    };
+  },
+  created() {
+    post_comment(this.$route.query.id).then((res) => {
+      this.comment = res.data.data;
+    });
+  },
 };
 </script>
 
 
 <style lang="less" scoped>
-/deep/.van-nav-bar{
-    border-bottom: 1px solid #ddd;
-    .van-icon{
-        color: #000;
-    }
-} 
+/deep/.van-nav-bar {
+  border-bottom: 1px solid #ddd;
+  .van-icon {
+    color: #000;
+  }
+}
 .item {
-    padding: 10px;
-    border-bottom: 1px solid #ccc;
-    .head {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        > img {
-            width: (50/360) * 100vw;
-            height: (50/360) * 100vw;
-            display: block;
-            border-radius: 50%;
-        }
-        > div {
-            flex: 1;
-            display: flex;
-            flex-direction: column;
-            margin-left: 10px;
-            > span {
-                font-size: 12px;
-                color: #999;
-                line-height: 25px;
-            }
-        }
-        > span {
-            color: #999;
-            font-size: 13px;
-        }
+  padding: 10px;
+  border-bottom: 1px solid #ccc;
+  .head {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    > img {
+      width: (50/360) * 100vw;
+      height: (50/360) * 100vw;
+      display: block;
+      border-radius: 50%;
     }
-    .text {
-        font-size: 14px;
-        color: #333;
-        padding: 20px 0 10px 0;
+    > div {
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      margin-left: 10px;
+      > span {
+        font-size: 12px;
+        color: #999;
+        line-height: 25px;
+      }
     }
+    > span {
+      color: #999;
+      font-size: 13px;
+    }
+  }
+  .text {
+    font-size: 14px;
+    color: #333;
+    padding: 20px 0 10px 0;
+  }
+}
+.commentItem {
+  border: 1px solid #ccc;
+  padding: 5px;
+  margin-top: 10px;
+  .top {
+    font-size: 12px;
+    color: #aaa;
+    display: flex;
+    justify-content: space-between;
+  }
+  .bottom {
+    font-size: 13px;
+    line-height: 40px;
+  }
 }
 </style>
