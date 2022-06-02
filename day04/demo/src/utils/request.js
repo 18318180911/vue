6603150b76c9ecd24fe9575c4392dffb74f6d15e
大聪明 <2119299531@qq.com>
2022-06-02 12:21:10 +0800

    响应拦截器的添加

--- day04/demo/src/utils/request.js
+++ day04/demo/src/utils/request.js
@@ -1,6 +1,7 @@
 // 封装接口基础路径
 import axios from 'axios'
 axios.defaults.baseURL = 'http://127.0.0.1:3000'
+import router from "@/router/index"
 // 添加请求拦截器
 axios.interceptors.request.use(function (config) {
     let token = localStorage.getItem('75-token')
@@ -10,6 +11,22 @@ axios.interceptors.request.use(function (config) {
     return config;
     }, function (error) {
         return Promise.reject(error)
+})
+// 添加响应式拦截器
+axios.interceptors.response.use(function (response) {
+    // 对响应数据做点什么
+    // console.log(25,response);
+    // 验证用户是否登录，未登录，则跳转到登录页面
+    // 未登录的情况下，后端会返回statusCode属性，当这个属性的值为401说用户信息验证失败
+    // 也就是没有登录
+    if(response.data.statusCode === 401) {
+        // 没有登录跳转到登录页面
+        router.push('/login')
+    }
+    return response;
+},function(error) {
+    // 对响应错误做点什么
+    return Promise.reject(error)
 })
 // 导出
 export default axios
