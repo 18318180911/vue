f706c9789efbdd20f16775e994e0e7389376a945
大聪明 <2119299531@qq.com>
2022-06-04 15:50:30 +0800

    state优化使用

--- vuex/demo1/src/App.vue
+++ vuex/demo1/src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
-    <!-- 设置一级挂载点 -->
-    <router-view></router-view>
+    <h1>渲染vuex中的count的数量为: {{count}}</h1>
   </div>
 </template>
 
 <script>
 export default {
-
+  // state变量的优化使用
+  computed: {
+    count() {
+      return this.$store.state.count
+    }
+  }
 }
 </script>
 
--- vuex/demo1/src/main.js
+++ vuex/demo1/src/main.js
@@ -8,7 +8,15 @@ import ElementUI  from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI)
 
+import Vuex from 'vuex'
+Vue.use(Vuex) 
+// 实例化
+const store = new Vuex.Store({
+  // state中存放的就是全局共享的数据
+  state: {count: 0}
+})
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
