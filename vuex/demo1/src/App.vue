39358ce2cf78fd1518a9bd44103b81cbf0022988
大聪明 <2119299531@qq.com>
2022-06-04 17:11:55 +0800

    mutations

--- vuex/demo1/src/App.vue
+++ vuex/demo1/src/App.vue
@@ -1,25 +1,33 @@
 <template>
   <div>
-    <h1>渲染vuex中的count的数量为: {{$store.state.count}}</h1>
-    <h1>渲染vuex中的count的数量为: {{count}}</h1>
-    <h1>渲染vuex中的count的数量为: {{count}}</h1>
-    <button @click="$store.commit('add', 5)">增加: {{count}}</button>
+    <h1>渲染vuex中的count的数量为: {{ $store.state.count }}</h1>
+    <h1>渲染vuex中的count的数量为: {{ count }}</h1>
+    <h1>渲染vuex中的count的数量为: {{ count }}</h1>
+    <button @click="$store.commit('add', 5)">增加: {{ count }}</button>
+    <button @click="addChange(6)">增加</button>
+    <button @click="add(7)">增加</button>
   </div>
 </template>
 
 <script>
-import {mapState} from 'vuex'
+import {mapState, mapMutations } from "vuex";
 export default {
   // state变量的优化使用
+
   computed: {
     count() {
-      return this.$store.state.count
+      return this.$store.state.count;
     },
-    ...mapState(['count'])
-  }
-}
+      ...mapState(["count"]),
+  },
+  methods: {
+    addChange(num) {
+      this.$store.commit("add", num);
+    },
+    ...mapMutations(['add'])
+  },
+};
 </script>
 
 <style>
-
 </style>
