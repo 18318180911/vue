548e9b1c9cd61b8cac6924f3bb5f1b989ff8f515
大聪明 <2119299531@qq.com>
2022-06-04 16:16:02 +0800

    state的使用和两种优化

--- vuex/demo1/src/App.vue
+++ vuex/demo1/src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div>
+    <h1>渲染vuex中的count的数量为: {{$store.state.count}}</h1>
+    <h1>渲染vuex中的count的数量为: {{count}}</h1>
     <h1>渲染vuex中的count的数量为: {{count}}</h1>
   </div>
 </template>
 
 <script>
+import {mapState} from 'vuex'
 export default {
   // state变量的优化使用
   computed: {
     count() {
       return this.$store.state.count
-    }
+    },
+    ...mapState(['count'])
   }
 }
 </script>
