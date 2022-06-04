2cde1062381bc012abc8bc6a82ba2af056fed62f
大聪明 <2119299531@qq.com>
2022-06-04 09:34:56 +0800

    清空脚手架的结构

---
+++ vuex/vuex-demo1/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>state的数据：{{ count }}</div>
+</template>
+
+<script>
+export default {
+computed: {
+  count() {
+    return this.$store.state.count
+  }
+}
+}
+</script>
+
+<style>
+
+</style>
